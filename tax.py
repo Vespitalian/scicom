2150d601d8fb00c2f50b3be11abc15adf26f4504
Vespitalian <vespitalian@gmail.com>
2025-09-13 22:04:54 -0700

    Shell version

--- tax.py
+++ tax.py
@@ -1,15 +1,16 @@
-def single_tax(pay):
-    tax_policy = [
-        [415050, .396], 
-        [413350, .35],
-        [190150, .33],
-        [91150, .28],
-        [37650, .25],
-        [9275, .15]
-    ]
-    tax = 0
-    for bracket in tax_policy:
-        if pay > bracket[0]:
-            tax += (pay - bracket[0]) * bracket[1]
-            pay = bracket[0]
-    return tax + pay * .1
+taxes = [
+    [9275, 0.1, 0.0],
+    [37650, 0.15, -463.75],
+    [91150, 0.25, -4228.75],
+    [190150, 0.28, -6963.25],
+    [413350, 0.33, -16470.75],
+    [415051, 0.35, -24737.75],
+]
+
+def single_tax(income):
+    # Check all brackets
+    for tax in taxes:
+        if income < tax[0]:
+            return income * tax[1] + tax[2]
+    # We calculated the top bracket earlier
+    return income * .396 + -43830.05
